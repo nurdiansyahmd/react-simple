7f9a37b41fa56f5156178e589fd08a464bcd8c84
nurdiansyahhamidi <nurdiansyahhamidi@gmail.com>
2022-12-02 14:07:57 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,84 +1,9 @@
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Outlet,
-  Navigate,
-} from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
-import LeftBar from "./components/leftBar/LeftBar";
-import RightBar from "./components/rightBar/RightBar";
-import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
-import "./style.scss";
-import { useContext } from "react";
-import { DarkModeContext } from "./context/darkModeContext";
-import { AuthContext } from "./context/authContext";
+import React from 'react'
 
-function App() {
-  const {currentUser} = useContext(AuthContext);
-
-  const { darkMode } = useContext(DarkModeContext);
-
-  const Layout = () => {
-    return (
-      <div className={`theme-${darkMode ? "dark" : "light"}`}>
-        <Navbar />
-        <div style={{ display: "flex" }}>
-          <LeftBar />
-          <div style={{ flex: 6 }}>
-            <Outlet />
-          </div>
-          <RightBar />
-        </div>
-      </div>
-    );
-  };
-
-  const ProtectedRoute = ({ children }) => {
-    if (!currentUser) {
-      return <Navigate to="/login" />;
-    }
-
-    return children;
-  };
-
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <ProtectedRoute>
-          <Layout />
-        </ProtectedRoute>
-      ),
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/profile/:id",
-          element: <Profile />,
-        },
-      ],
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-  ]);
-
+const App = () => {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
